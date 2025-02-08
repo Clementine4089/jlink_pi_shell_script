77f667ded2c7a162eff194c8a4489aa015dea00b
Clementine Allen <callen42@uw.edu>
2025-02-08 12:09:01 -0800

    Updated script with stupiud aded

--- setup_jlink.sh
+++ setup_jlink.sh
@@ -1,27 +1,37 @@
 #!/bin/bash
 
-JLINK_VERSION="V812g"
+JLINK_VERSION="V646g"
+USER="aruw"
 JLINK_DIR="JLink_Linux_${JLINK_VERSION}_arm"
 JLINK_TGZ="JLink_Linux_arm.tgz"
-JLINK_DOWNLOAD_URL="https://www.segger.com/downloads/jlink/JLink_Linux_arm.tgz"
-JLINK_EXEC="$HOME/$JLINK_DIR/JLinkRemoteServerCLExe"
+JLINK_DOWNLOAD_URL="https://www.segger.com/downloads/jlink/JLink_Linux_${JLINK_VERSION}_arm.tgz"
+JLINK_EXEC="home/$user/$JLINK_DIR/JLinkRemoteServerCLExe"
 JLINK_SERVICE_FILE="/etc/systemd/system/jlink-remote-server.service"
 
 echo "Starting J-Link setup for Raspberry Pi..."
 
-if [[ ! -f "$JLINK_TGZ" ]]; then
-    echo "Downloading J-Link utilities..."
-    wget --post-data 'accept_license_agreement=accepted&non_emb_ctr=confirmed&submit=Download+software' "$JLINK_DOWNLOAD_URL" -O "$JLINK_TGZ"
-
-else
-    echo "J-Link archive already exists. Skipping download."
+# Check if the required 32-bit dynamic linker exists.
+if [ ! -f /lib/ld-linux-armhf.so.3 ]; then
+    echo "32-bit dynamic linker /lib/ld-linux-armhf.so.3 not found."
+    echo "Enabling armhf architecture and installing required 32-bit libraries..."
+    sudo dpkg --add-architecture armhf
+    sudo apt update
+    sudo apt install -y libc6:armhf
+    echo "Creating symlink for 32-bit dynamic linker..."
+    sudo ln -s /lib/arm-linux-gnueabihf/ld-linux-armhf.so.3 /lib/ld-linux-armhf.so.3
 fi
+
+echo "Downloading J-Link utilities..."
+wget --post-data 'accept_license_agreement=accepted&non_emb_ctr=confirmed&submit=Download+software' "$JLINK_DOWNLOAD_URL" -O "$JLINK_TGZ"
 
 echo "Extracting J-Link archive..."
 tar xvf "$JLINK_TGZ"
-
+cd "$JLINK_DIR"
 echo "Installing udev rules..."
-sudo cp "$JLINK_DIR/99-jlink.rules" /etc/udev/rules.d/
+sudo cp 99-jlink.rules /etc/udev/rules.d/
+sudo udevadm control -R
+sudo udevadm trigger --action=remove --attr-match=idVendor=1366 --subsystem-match=usb
+sudo udevadm trigger --action=add    --attr-match=idVendor=1366 --subsystem-match=usb
 
 IS_PI_ZERO=$(cat /proc/cpuinfo | grep "Hardware" | grep -q "BCM2835" && echo "yes" || echo "no")
 
@@ -42,9 +52,9 @@ After=network.target
 [Service]
 Type=simple
 ExecStart=$JLINK_EXEC -Port 19020
-WorkingDirectory=$HOME
-User=pi
-Group=pi
+WorkingDirectory=/home/$USER/
+User=$USER
+Group=$USER
 Restart=on-failure
 
 [Install]
